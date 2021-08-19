eb2fdb41c226ee8de9559b4fcadab7f9559e4b29
Byrne Reese <byrne@majordojo.com>
2021-08-19 13:54:42 -0700

    Updated command line args

--- src/mkdocs_linkcheck/__main__.py
+++ src/mkdocs_linkcheck/__main__.py
@@ -14,25 +14,45 @@ from .base import check_links
 
 def main():
     p = argparse.ArgumentParser(description="Check links in markdown files common to mkdocs-generated websites")
-    p.add_argument("path", help="Path to markdown files")
+    p.add_argument(
+        "path",
+        help="Path to markdown files")
     p.add_argument(
         "domain",
         help="check only links to this domain (say github.com without https etc.)",
-        nargs="?",
-    )
-    p.add_argument("-ext", help="file extension to scan", default=".md")
+        nargs="?")
     p.add_argument(
-        "-m",
-        "--method",
+        "--ext",
+        help="file extension to scan",
+        default=".md")
+    p.add_argument(
+        "-m","--method",
         choices=["get", "head"],
         help="head is faster but gives false positives. Get is reliable but slower",
-        default="get",
-    )
-    p.add_argument("-v", "--verbose", action="store_true")
-    p.add_argument("--sync", help="don't use asyncio", action="store_true")
-    p.add_argument("--exclude", help="a pattern for a file or path to exclude", action="append")
-    p.add_argument("-local", help="only check local files", action="store_true")
-    p.add_argument("-r", "--recurse", help="recurse directories under path", action="store_true")
+        default="get")
+    p.add_argument(
+        "-v", "--verbose",
+        action="store_true")
+    p.add_argument(
+        "--sync",
+        help="don't use asyncio",
+        action="store_true")
+    p.add_argument(
+        "--exclude",
+        help="a pattern for a file or path to exclude",
+        action="append")
+    p.add_argument(
+        "--method",
+        help="The HTTP method to use when checking external links",
+        default="get")
+    p.add_argument(
+        "-l","--local",
+        help="only check local files",
+        action="store_true")
+    p.add_argument(
+        "-r", "--recurse",
+        help="recurse directories under path",
+        action="store_true")
     P = p.parse_args()
 
     if P.verbose:
